7f8de6a8b30182e2f9ceca2b8ecbcf6944e3397d
Junhyeok <cjh64866@naver.com>
2025-03-27 02:07:59 +0900

    FEAT: OS 테마 감지 추가

--- week02/mission02/src/contexts/ThemeContext.tsx
+++ week02/mission02/src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 interface ThemeContextProps {
   theme: string;
@@ -10,7 +10,29 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 type theme = "light" | "dark";
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<theme>("light");
+  const [theme, setTheme] = useState<theme>(() => {
+    // os 설정에 따라 init theme 설정
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
+    }
+  });
+
+  // theme 변경 감지
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleChangeTheme = (event: MediaQueryListEvent) => {
+      setTheme(event.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", handleChangeTheme);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChangeTheme);
+    };
+  }, []);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
