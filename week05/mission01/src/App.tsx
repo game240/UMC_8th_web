ac54ac36b69363765eb60ac9285d3fef2862571e
Junhyeok <cjh64866@naver.com>
2025-04-29 18:22:03 +0900

    feat: 네비게이션 바 routing 추가 및 랜딩 페이지 추가

--- week05/mission01/src/App.tsx
+++ week05/mission01/src/App.tsx
@@ -5,6 +5,7 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Mypage from "./pages/Mypage";
+import Landing from "./pages/Landing";
 
 import ProtectedRoute from "./components/route/ProtectedRoute";
 
@@ -15,6 +16,8 @@ const router = createBrowserRouter([
     path: "/",
     element: <DefaultLayout />,
     children: [
+      { index: true, element: <Landing /> },
+
       {
         path: "login",
         element: <Login />,
--- week05/mission01/src/components/NavBar.tsx
+++ week05/mission01/src/components/NavBar.tsx
@@ -1,10 +1,62 @@
+import { useContext } from "react";
+
+import AuthContext from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 const NavBar = () => {
+  const { isAuthenticated, signOut } = useContext(AuthContext)!;
+  const navigate = useNavigate();
+
   return (
     <nav className="flex justify-between items-center px-8 w-full h-16 bg-[#111]">
-      <p className="text-3xl text-rose-900 font-extrabold">돌려돌려 LP판</p>
+      <div className="relative">
+        <p className="text-3xl text-rose-900 font-extrabold">돌려돌려 LP판</p>
+        <button
+          className="absolute top-0 left-0 size-full"
+          onClick={() => {
+            navigate("/");
+          }}
+        ></button>
+      </div>
       <div className="flex gap-4">
-        <button className="w-[80px] h-[36px] rounded-[8px] text-white bg-black">로그인</button>
-        <button className="w-[80px] h-[36px] rounded-[8px] text-white bg-rose-900">회원가입</button>
+        {!isAuthenticated ? (
+          <>
+            <button
+              className="w-[80px] h-[36px] rounded-[8px] text-white bg-black"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              로그인
+            </button>
+            <button
+              className="w-[80px] h-[36px] rounded-[8px] text-white bg-rose-900"
+              onClick={() => navigate("/signup")}
+            >
+              회원가입
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="w-[80px] h-[36px] rounded-[8px] text-white bg-black"
+              onClick={() => {
+                navigate("/mypage");
+              }}
+            >
+              마이페이지
+            </button>
+            <button
+              className="w-[80px] h-[36px] rounded-[8px] text-white bg-rose-900"
+              onClick={() => {
+                navigate("/");
+                signOut();
+              }}
+            >
+              로그아웃
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
