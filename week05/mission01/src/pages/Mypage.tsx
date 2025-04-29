bad374bacaa612643cdc938917c2b6021a6363b4
Junhyeok <cjh64866@naver.com>
2025-04-29 17:22:56 +0900

    feat: Mypage 토큰 필요한 API 요청

--- week05/mission01/src/pages/Mypage.tsx
+++ week05/mission01/src/pages/Mypage.tsx
@@ -1,7 +1,21 @@
+import { AxiosError } from "axios";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import axiosClient from "../services/api";
 
 const Mypage = () => {
   const { getItem: getName } = useLocalStorage("name");
+
+  try {
+    axiosClient.get("/v1/auth/protected");
+    alert("내 정보 조회 성공!");
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      alert(error?.response?.data.message);
+    } else {
+      alert("내 정보 조회에 실패했습니다.");
+    }
+  }
+
   return (
     <main className="w-full p-20">
       <section className="flex flex-col items-center">
