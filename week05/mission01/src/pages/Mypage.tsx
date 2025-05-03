4e36adedb54528ac133fc88b475b0c4c8c3d9b02
Junhyeok <cjh64866@naver.com>
2025-05-03 13:26:55 +0900

    fix: Mypage 본문에 api 요청 -> useEffect로 변경

--- week05/mission01/src/pages/Mypage.tsx
+++ week05/mission01/src/pages/Mypage.tsx
@@ -1,19 +1,28 @@
 import { AxiosError } from "axios";
+import { useEffect } from "react";
+
 import { useLocalStorage } from "../hooks/useLocalStorage";
+
 import axiosClient from "../services/api";
 
 const Mypage = () => {
   const { getItem: getName } = useLocalStorage("name");
 
-  try {
-    axiosClient.get("/v1/auth/protected");
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      alert(error?.response?.data.message);
-    } else {
-      alert("내 정보 조회에 실패했습니다.");
-    }
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await axiosClient.get("/v1/auth/protected");
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          alert(error?.response?.data.message);
+        } else {
+          alert("내 정보 조회에 실패했습니다.");
+        }
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main className="w-full p-20">
